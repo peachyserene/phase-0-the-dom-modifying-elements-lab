01a3c74807787c2313e70de70a08e954c5af9b09
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-03-27 20:57:46 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+//newHeader.nodeName = "h1"
+newHeader.id = 'victory'
+//newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Peachy is the champion!";
+document.body.append(newHeader);
